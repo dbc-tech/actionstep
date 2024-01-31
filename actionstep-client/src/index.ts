b85b8616fd89b2c07c2ee4b15bd2bc2c29168250
Neil Dobson <neil.dobson71@gmail.com>
2024-01-31 21:23:17 +1100

    Add store support

--- actionstep-client/src/index.ts
+++ actionstep-client/src/index.ts
@@ -1,4 +1,4 @@
-import { AuthorizationCode } from 'simple-oauth2'
+import { AccessToken, AuthorizationCode, Token } from 'simple-oauth2'
 
 export type ActionStepClientConfig = {
   client_id: string
@@ -7,13 +7,18 @@ export type ActionStepClientConfig = {
   token_url: string
   scope?: string | string[] | undefined
   redirect_uri: string
+  store?: TokenStore
 }
 
-export type HandleCallbackOptions = { code: string }
+export type TokenStore = {
+  get: () => Promise<Token>
+  set: (token: Token) => Promise<void>
+}
 
 export const ActionStepAuth = (config: ActionStepClientConfig) => {
   const authorizeURL = new URL(config.authorize_url)
   const tokenURL = new URL(config.token_url)
+  let accessToken: AccessToken
 
   const authCode = new AuthorizationCode({
     client: {
@@ -30,22 +35,34 @@ export const ActionStepAuth = (config: ActionStepClientConfig) => {
   })
 
   return {
-    authorizeURL: () => {
+    authorizeUrl: (): string => {
       return authCode.authorizeURL({
         redirect_uri: config.redirect_uri,
         scope: config.scope,
       })
     },
-    handleCallback: async ({ code }: HandleCallbackOptions) => {
-      const { token } = await authCode.getToken({
+    callback: async (code: string): Promise<Token> => {
+      accessToken = await authCode.getToken({
         code,
         redirect_uri: config.redirect_uri,
         scope: config.scope,
       })
 
-      // TODO: Store token
+      if (config.store) await config.store.set(accessToken.token)
 
-      return token
+      return accessToken.token
+    },
+    token: async (): Promise<Token> => {
+      if (config.store) {
+        accessToken = authCode.createToken(await config.store.get())
+      }
+
+      if (accessToken.expired()) {
+        accessToken = await accessToken.refresh()
+        if (config.store) await config.store.set(accessToken.token)
+      }
+
+      return accessToken.token
     },
   }
 }
