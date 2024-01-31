8a63596b3a512aed8822039616bd19253d8db022
Neil Dobson <neil.dobson71@gmail.com>
2024-02-01 10:06:43 +1100

    scaffold

--- actionstep-client/src/action-step-client.ts
+++ actionstep-client/src/action-step-client.ts
@@ -2,7 +2,14 @@ import { ActionStepAuth } from './types/action-step-auth.type'
 
 export const actionStepClient = (auth: ActionStepAuth) => {
   return {
-    get: async (): Promise<void> => {
+    // TODO: Create an internal HTTP client which handles auth / refresh (on 404) by calling auth.token()
+    getAction: async (): Promise<void> => {
+      // TODO: Implement
+      const token = await auth.token()
+      console.log(token)
+    },
+    createFile: async (): Promise<void> => {
+      // TODO: Implement
       const token = await auth.token()
       console.log(token)
     },
