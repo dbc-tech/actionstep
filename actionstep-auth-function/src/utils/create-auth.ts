b945cd838cc861fa4c7e5a94fdf3d6e6d97cd60e
Neil Dobson <neil.dobson71@gmail.com>
2024-02-04 19:47:57 +1100

    Remove client dependency

--- actionstep-auth-function/src/utils/create-auth.ts
+++ actionstep-auth-function/src/utils/create-auth.ts
@@ -3,26 +3,26 @@ import { blobInput, blobOutput } from '../io/blob.io'
 import {
   ActionStepClientConfig,
   ActionStepToken,
-  actionStepAuth,
-} from '@dbc-tech/actionstep'
-
+} from '../../../actionstep-client/src/types'
+import { actionStepAuth } from '../../../actionstep-client/src'
 export const createAuth = (
   request: HttpRequest,
   context: InvocationContext,
 ) => {
   const url = new URL(request.url)
   const config: ActionStepClientConfig = {
-    authorize_url: process.env.ACTIONSTEP_AUTHORIZE_URL!,
-    client_id: process.env.ACTIONSTEP_CLIENT_ID!,
-    client_secret: process.env.ACTIONSTEP_CLIENT_SECRET!,
+    authorize_url: process.env.ACTIONSTEP_AUTHORIZE_URL,
+    client_id: process.env.ACTIONSTEP_CLIENT_ID,
+    client_secret: process.env.ACTIONSTEP_CLIENT_SECRET,
     redirect_uri: url.origin + url.pathname,
-    token_url: process.env.ACTIONSTEP_TOKEN_URL!,
+    token_url: process.env.ACTIONSTEP_TOKEN_URL,
     store: {
       get: () =>
         Promise.resolve(<ActionStepToken>context.extraInputs.get(blobInput)),
       set: (token) =>
         Promise.resolve(context.extraOutputs.set(blobOutput, token)),
     },
+    api_url: process.env.ACTIONSTEP_API_URL,
   }
 
   return actionStepAuth(config)
