84e1fb43b8d8c429c8dc35cf46ba503265c84f0a
Neil Dobson <neil.dobson71@gmail.com>
2024-02-04 19:12:22 +1100

    Connection string from env var

--- actionstep-auth-function/src/io/blob.io.ts
+++ actionstep-auth-function/src/io/blob.io.ts
@@ -1,11 +1,11 @@
 import { input, output } from '@azure/functions'
 
 export const blobInput = input.storageBlob({
-  path: process.env.ACTIONSTEP_TOKEN_BLOB_PATH!,
-  connection: 'AzureWebJobsStorage',
+  path: process.env.ACTIONSTEP_TOKEN_BLOB_PATH,
+  connection: process.env.ACTIONSTEP_STORAGE_CONNECTION,
 })
 
 export const blobOutput = output.storageBlob({
-  path: process.env.ACTIONSTEP_TOKEN_BLOB_PATH!,
-  connection: 'AzureWebJobsStorage',
+  path: process.env.ACTIONSTEP_TOKEN_BLOB_PATH,
+  connection: process.env.ACTIONSTEP_STORAGE_CONNECTION,
 })
