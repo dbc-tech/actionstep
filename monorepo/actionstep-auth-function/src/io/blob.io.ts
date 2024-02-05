9c261257d5997aa738756ed73fd1b421b5fdbe8c
Neil Dobson <neil.dobson71@gmail.com>
2024-02-05 21:39:56 +1100

    Fix storage connection

--- monorepo/actionstep-auth-function/src/io/blob.io.ts
+++ monorepo/actionstep-auth-function/src/io/blob.io.ts
@@ -2,10 +2,10 @@ import { input, output } from '@azure/functions'
 
 export const blobInput = input.storageBlob({
   path: process.env.ACTIONSTEP_TOKEN_BLOB_PATH,
-  connection: process.env.ACTIONSTEP_STORAGE_CONNECTION,
+  connection: 'ACTIONSTEP_STORAGE_CONNECTION',
 })
 
 export const blobOutput = output.storageBlob({
   path: process.env.ACTIONSTEP_TOKEN_BLOB_PATH,
-  connection: process.env.ACTIONSTEP_STORAGE_CONNECTION,
+  connection: 'ACTIONSTEP_STORAGE_CONNECTION',
 })
