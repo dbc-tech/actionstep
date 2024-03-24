baf8d0c286a398db5a431e139364e4fa392b0ebc
Neil Dobson <neil.dobson71@gmail.com>
2024-03-24 11:00:46 +1100

    Move force refresh into separate handler

--- monorepo/actionstep-auth-function/src/handlers/refresh.handler.ts
+++ monorepo/actionstep-auth-function/src/handlers/refresh.handler.ts
@@ -5,13 +5,13 @@ import {
 } from '@azure/functions'
 import { createAuth } from '../utils/create-auth'
 
-export async function tokenHandler(
+export async function refreshHandler(
   request: HttpRequest,
   context: InvocationContext,
 ): Promise<HttpResponseInit> {
   const auth = createAuth(request, context)
 
   return {
-    jsonBody: await auth.token(),
+    jsonBody: await auth.token(true),
   }
 }
